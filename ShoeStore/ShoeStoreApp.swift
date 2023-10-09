893a0551c64df28f96545003becac2c44ab353af
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-10-09 12:24:44 +0300

    Initial Commit

---
+++ ShoeStore/ShoeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeStoreApp.swift
+//  ShoeStore
+//
+//  Created by Y K on 09.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
